0eebe50d55013029c94d6b11b56dcfa09aa3f51c
dmchavira1 <Pizza101@>
2020-02-19 14:44:52 -0700

    Snakegame has finished constructors

--- SnakeGame/src/SnakeGame.java
+++ SnakeGame/src/SnakeGame.java
@@ -1,3 +1,40 @@
 public class SnakeGame {
+    //attributes
+    private boolean [][] game;
+    private int [] headPosition = new int [2];
+    private static int exhaustiveChecks;
+    private static int recursiveChecks;
+    //constructors
+    public SnakeGame(){
+        game = new boolean[1][1];
+    }
+    public SnakeGame(boolean [][] A, int x, int y){
+        for(int i = 0; i < A.length;i++ ){
+            for(int j = 0; j < A[i].length;j++){
+                game[i][j] = A[i][j];
+            }
+        }
+        headPosition [0] = x;
+        headPosition [1] = y;
 
+    }
+    //methods
+    public int [] findTailExhaustive() {
+
+
+    }
+    public int [] findTailRecursion(){
+    }
+    public int [] findTailRecursion(int [] currentPosition, int [] previousPosition){
+
+    }
+    private void resetCounters(){
+
+    }
+    private static int getRecursiveChecks(){
+
+    }
+    private static int getExhaustiveChecks(){
+
+    }
 }

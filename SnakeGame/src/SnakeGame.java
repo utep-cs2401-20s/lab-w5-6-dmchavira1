e6dcd65676749204f18035b2875c1d48f7b4b157
dmchavira1 <Pizza101@>
2020-02-28 21:17:51 -0700

    finished lab snake assignment

--- SnakeGame/src/SnakeGame.java
+++ SnakeGame/src/SnakeGame.java
@@ -4,100 +4,126 @@ public class SnakeGame {
     private int[] headPosition = new int[2];
     private static int exhaustiveChecks;
     private static int recursiveChecks;
-
     //constructors
+    //constructor that makes a empty 1x1 board
     public SnakeGame() {
         game = new boolean[1][1];
     }
-
+    // i had jessie olivas help me with this part.this constructor take a 2D array that is boolean and the position of x and y of the snakes head
     public SnakeGame(boolean[][] A, int x, int y) {
+        headPosition= new int[2];
+        game = new boolean[A.length][A.length];
         for (int i = 0; i < A.length; i++) {
             for (int k = 0; k < A[i].length; k++) {
                 game[i][k] = A[i][k];
             }
         }
-        headPosition= new int[2];
         headPosition[0] = x;
         headPosition[1] = y;
-
     }
-
     //methods
+    //this method should return where x and y are in the board
     public int[] findTailExhaustive() {
         resetCounters();
-        int[] tail = new int[3];
-        int length = 0;
-
+        int[] t = new int[3];
+        int snakeLength = 0;
+        // used a for loop to check  where the snake is at or
+        // which position the snake is.
         for (int i = 0; i < game.length; i++) {
             for (int k = 0; k < game[i].length; k++) {
                 exhaustiveChecks++;
                 if (game[i][k]) {
-                    length++;
-                    if (headPosition[0] != i && headPosition[1] != k && neighbors(i, k) == 1) {
-                        tail[0] = i;
-                        tail[1] = k;
-                        tail[2] = length;
-                        return tail;
+                    snakeLength++;
+                    if (neighbors(i, k) == 1 && headPosition[0] != i || headPosition[1] != k) {
+                        t[0] = i;
+                        t[1] = k;
                     }
                 }
             }
         }
-
-        return tail;
+        t[2] = snakeLength;
+        return t;
     }
-
-
     public int[] findTailRecursive() {
-
+        //used encapsulation to return the x and y position and its length.
         resetCounters();
-        int[] tail = new int[3];
-        for(int i = 0; i < game.length;i++){
-            for(int j = 0; j < game[i].length;j++){
-
-            }
-
-        }
-
-
-        return tail;
+        int[] t = findTailRecursive(headPosition, headPosition);
+        int[] snakeTail = new int[3];
+        snakeTail[0] = t[0];
+        snakeTail[1] = t[1];
+        snakeTail[2] = length();
+        return snakeTail;
     }
-
-
-
+//allows the snake to move up, down, left, right.
     public int neighbors(int r, int c) {
         int count = 0;
-        if ((c + 1 < game.length) && (game[r][c + 1])) {
+        //right
+        if (c + 1 != game.length && game[r][c + 1]) {
             count++;
         }
-        if (r + 1 < game.length && game[r + 1][c]) {
+        //left
+        if (c - 1 > 0 && game[r][c - 1]) {
             count++;
         }
-        if (c - 1 >= 0 && c > 0 && game[r][c - 1]) {
+        //bottom
+        if (r + 1 !=game.length && game[r + 1][c]) {
             count++;
         }
-        if (r - 1 >= 0 && game[r - 1][c]) {
-            count++;
-        }
-        if (r - 1 >= 0 && c + 1 < game.length && game[r - 1][c + 1]) {
-            count++;
-        }
-        if (r + 1 < game.length && c + 1 < game.length && game[r + 1][c + 1]) {
-            count++;
-        }
-        if (r + 1 < game.length && c - 1 >= 0 && game[r + 1][c - 1]) {
-            count++;
-        }
-        if (r - 1 >= 0 && c - 1 >= 0 && game[r - 1][c - 1]) {
+        //top
+        if (r - 1 > 0 && game[r - 1][c]) {
             count++;
         }
         return count;
     }
-
-    public int [] findTailRecursion(int [] currentPosition, int [] previousPosition){
-
-    return currentPosition;
+    public int length(){
+        int count = 0;
+        for (boolean[] booleans : game) {
+            for (boolean aBoolean : booleans) {
+                if (aBoolean) {
+                    count++;
+                }
+            }
+        }
+        return count;
     }
-    private void resetCounters(){
+    private int [] findTailRecursive(int [] currentPosition, int [] previousPosition){
+        int[] afterPosition = new int[2];
+        afterPosition[0] = currentPosition[0];
+        afterPosition[1] = currentPosition[1];
+        //diana valles and jessie olivas had helped me with this part.
+        if((afterPosition[0] == headPosition[0] && afterPosition[1] != headPosition[1])||(afterPosition[0] != headPosition[0] && afterPosition[1] == headPosition[1])|| ((afterPosition[0] != headPosition[0] && afterPosition[1] != headPosition[1]) && neighbors(afterPosition[0],afterPosition[1]) == 1)){
+            if(neighbors(afterPosition[0],afterPosition[1]) == 1) {
+                recursiveChecks++;
+                return currentPosition;
+            }
+        }
+        if((currentPosition[0] + 1 < game.length) && (currentPosition[0] + 1 != previousPosition[0]) && (game[currentPosition[0] + 1][currentPosition[1]])){
+                    afterPosition[0] = currentPosition[0] + 1;
+                    afterPosition[1] = currentPosition[1];
+                    recursiveChecks++;
+                    return findTailRecursive(previousPosition, currentPosition);
+                }
+        if((currentPosition[0] - 1 >= 0) && (currentPosition[0] - 1 != previousPosition[0]) && (game[currentPosition[0] - 1][currentPosition[1]])) {
+                    afterPosition [0] = currentPosition[0] - 1;
+                    afterPosition [1] = currentPosition[1];
+                    recursiveChecks++;
+                    return findTailRecursive(afterPosition, currentPosition);
+                }
+        if((currentPosition[1] + 1 < game.length) && (currentPosition[1] + 1 != previousPosition[1])&& (game[currentPosition[0]][currentPosition[1] + 1])){
+                    afterPosition[0] = currentPosition[0];
+                    afterPosition[1] = currentPosition[1] + 1;
+                    recursiveChecks++;
+                    return findTailRecursive(afterPosition, currentPosition);
+                }
+        if((currentPosition[1] - 1 >= 0) && (currentPosition[1] - 1 != previousPosition[1]) && (game[currentPosition[0]][currentPosition[1] - 1])){
+                    afterPosition[0] = currentPosition[0];
+                    afterPosition[1] = currentPosition[1] - 1;
+                    recursiveChecks++;
+                    return findTailRecursive(afterPosition, currentPosition);
+                }
+        return new int[]{currentPosition[0], currentPosition[1], 1};
+        }
+        private void resetCounters(){
         recursiveChecks = 0;
         exhaustiveChecks = 0;
 
--- SnakeGame/src/SnakeGameTester.java
+++ SnakeGame/src/SnakeGameTester.java
@@ -1,6 +1,154 @@
 import org.junit.Test;
 
-public class SnakeGameTester {
-    @Test
+import static org.junit.Assert.assertArrayEquals;
 
-}
+public class SnakeGameTester {
+    /* this test is just to see if everything is running up to par and if the code is working.
+    and testing one of the images from the read me would allow it to pass.
+    TEST STATUS: Passed
+    */
+    @Test
+    public void exhaustiveTest1(){
+        boolean[][] game = {
+                {false,false,false,false,false},
+                {false,false,true,false,false},
+                {false,false,true,false,false},
+                {false,false,true,false,false},
+                {false,false,false,false,false}};
+        int[] z = {3,2,3};
+        SnakeGame t = new SnakeGame(game,1,2);
+        assertArrayEquals(z, t.findTailExhaustive());
+    }
+    /*
+    this test was to see what would happen if the game board was smaller and if the snake was diagonal,
+     */
+    @Test
+    public void exhaustiveTest2(){
+        boolean[][] game = {
+                {false,false,false,},
+                {true,true,true,},
+                {false,false,false,}};
+        int[] z = {1,1,3};
+        SnakeGame t = new SnakeGame(game,1,2);
+        assertArrayEquals(z, t.findTailExhaustive());
+    }
+    @Test
+    /*
+    this test was to see if it can find the tail if it was turning.
+     */
+
+    public void exhaustiveTest3(){
+        boolean[][] game = {
+                {false,false,false,false,false},
+                {false,false,true,false,false},
+                {false,false,true,false,false},
+                {false,true,true,false,false},
+                {false,false,false,false,false}};
+        int[] z = {3,1,4};
+        SnakeGame t = new SnakeGame(game,1,2);
+        assertArrayEquals(z, t.findTailExhaustive());
+    }
+    @Test
+    /*
+    this test was to see if it was able to detect the tail if it was turnt and long
+    passed
+     */
+    public void exhaustiveTest4(){
+        boolean[][] game = {
+                {false,false,false,false,false},
+                {false,false,true,false,false},
+                {false,false,true,false,false},
+                {true,true,true,false,false},
+                {false,false,false,false,false}};
+        int[] z = {3,2,5};
+        SnakeGame t = new SnakeGame(game,0,1);
+        assertArrayEquals(z, t.findTailExhaustive());
+    }
+    @Test
+    /*
+    this was to test the tail in a u-shape
+     */
+    public void exhaustiveTest5(){
+        boolean[][] game = {
+                {false,false,false,false,false},
+                {false,false,false,false,false},
+                {false,true,false,true,false},
+                {false,true,true,true,false},
+                {false,false,false,false,false}};
+        int[] z = {3,3,5};
+        SnakeGame t = new SnakeGame(game,2,2);
+        assertArrayEquals(z, t.findTailExhaustive());
+    }
+    @Test
+    /*
+    this test was to see what would happened if it found its tail
+     */
+
+    public void recursiveTest1(){
+        boolean[][] game = {
+                {false,false,false,false,false},
+                {false,false,false,false,false},
+                {false,false,false,false,false},
+                {false,false,false,false,false},
+                {true,true,true,true,true}};
+        int[] z = {4,1,5};
+        SnakeGame t = new SnakeGame(game,4,0);
+        assertArrayEquals(z, t.findTailRecursive());
+    }
+    @Test
+    //this test was to see if the method is functioning like its supposed to and see if it work
+    public void recursiveTest2(){
+        boolean[][] game = {
+                {true,false,false,false,false},
+                {true,false,false,false,false},
+                {true,false,false,false,false},
+                {true,false,false,false,false},
+                {true,false,false,false,false}};
+        int[] z = {1,0,5};
+        SnakeGame t = new SnakeGame(game,0,0);
+        assertArrayEquals(z, t.findTailRecursive());
+    }
+    @Test
+    // this test was to see if the head is facing towards the head
+    public void recursiveTest3(){
+        boolean[][] game = {
+                {false,false,false,false,false},
+                {false,true,true,true,false},
+                {false,true,false,true,false},
+                {false,false,true,true,false},
+                {false,false,true,true,false}};
+        int[] z = {4,0,9};
+        SnakeGame t = new SnakeGame(game,4,0);
+        assertArrayEquals(z, t.findTailRecursive());
+    }
+    @Test
+    // testing if the snake if it was going to be out of bounds
+    public void recursiveTest4(){
+        boolean[][] game = {
+                {true,true,true,true,false},
+                {false,false,false,false,false},
+                {false,false,false,false,false},
+                {false,false,false,false,false},
+                {false,false,false,false,false}};
+        int[] z = {4,0,4};
+        SnakeGame t = new SnakeGame(game,0,0);
+        assertArrayEquals(z, t.findTailRecursive());
+    }
+    @Test
+    //this code was to see if the snake would run even if its just new
+    public void recursiveTest5(){
+        boolean[][] game = {
+                {false,false,false,false,false},
+                {false,false,false,false,false},
+                {false,true,true,true,false},
+                {false,false,false,false,false},
+                {false,false,false,false,false}};
+        int[] z = {2,3,3};
+        SnakeGame t = new SnakeGame(game,2,1);
+        assertArrayEquals(z, t.findTailRecursive());
+    }
+
+    }
+
+
+
